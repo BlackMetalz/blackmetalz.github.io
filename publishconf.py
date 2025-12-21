c9be95dbf4602f9ccd270a7cf377a0444fa5782a
kienlt <kienlt.qn@gmail.com>
2025-12-21 22:19:47 +0700

    Fix GitHub Pages deployment: use publishconf.py and correct SITEURL with https://

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "blackmetalz.github.io"
+SITEURL = "https://blackmetalz.github.io"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
